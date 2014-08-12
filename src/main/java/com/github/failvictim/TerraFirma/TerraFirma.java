be5c36ae8b2fbed47a455b2013282bf08d2f327d
Elliot Millington <elliot.millington1@gmail.com>
2014-08-12 17:50:00 +0100

    Basic Plugin Functionality
    Add key Bukkit infrastructure

---
+++ src/main/java/com/github/failvictim/TerraFirma/TerraFirma.java
@@ -0,0 +1,16 @@
+package com.github.failvictim.TerraFirma;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TerraFirma extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+			//TODO All this Jazz
+		}
+	
+	public void onDisable() {
+		//TODO All this Jazz
+	}
+		
+}
